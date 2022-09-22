f6fa1d4681b9f0024c090c271fa1cac7673e915a
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2022-09-22 02:28:55 -0700

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -2,8 +2,17 @@ from swallowtail.operation import operator as op
 from swallowtail import parsers
 from swallowtail.program import Program
 
-image = Program(parsers.DLL('examples/HelloWorld_x86.exe'))
+image = Program(parsers.EXE('examples/HelloWorld_x86.exe'))
 
+'''
+this instruction:
+MOV dst, src
+
+is equivalent to these instructions:
+PUSH src
+POP dst
+
+but both operations have entirely different binary signatures.
 '''
 @image.mutator(op.Move)
 def mov_to_pushpop(mov):
@@ -12,14 +21,21 @@ def mov_to_pushpop(mov):
         op.Pop(mov.dst)
     )
 
+'''
+Same idea as above, but backwards.
+'''
 @image.mutator(op.Push, op.Pop)
 def pushpop_to_mov(push, pop):
     return (
         op.Move(src=push.target, dst=pop.target)
     )
 
-image.mutate()
+stats = image.mutate()
 image.save('modified.exe')
-'''
 
-print(image.parser.prettify())
+print(stats.prettify())
+# ^ shows details about:
+# - how many mutations occurred
+# - how much code is left unchanged
+# - where there are opportunities for more mutations (i.e. where there are long sequences of unmutated code)
+# - ... and tons of other stuff
